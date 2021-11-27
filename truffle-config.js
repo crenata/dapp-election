4c89cdf140357f565bd02d7860b5c428f6e2b534
root <root@localhost.localdomain>
2021-11-27 10:06:05 +0700

    Compiles assets for Github Pages

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,17 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // for more about customizing your Truffle configuration!
     networks: {
+        ropsten: {
+            provider: () => new HDWalletProvider({
+                mnemonic: process.env.MNEMONIC,
+                providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+            }),
+            network_id: 3
+        },
         ganache_gui: {
             host: "127.0.0.1",
             port: 7545,
